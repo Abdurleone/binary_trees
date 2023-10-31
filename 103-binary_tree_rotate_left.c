321c9929d2441699d6f13e6ebd073733c9663b4e
Abdurleone <abyyyusuf@gmail.com>
2023-10-31 13:02:05 +0300

    binary

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -8,20 +8,20 @@
 
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
-    binary_tree_t *pivot;
+	binary_tree_t *pivot;
 
-    if (tree == NULL || tree->right == NULL)
-    {
-        return (NULL);
-    }
-    pivot = tree->right;
-    tree->right = pivot->left;
-    if (pivot->left != NULL)
-    {
-        pivot->left->parent = tree;
-    }
-    pivot->left = tree;
-    pivot->parent = tree->parent;
-    tree->parent = pivot;
-    return (pivot);
+	if (tree == NULL || tree->right == NULL)
+	{
+		return (NULL);
+	}
+	pivot = tree->right;
+	tree->right = pivot->left;
+	if (pivot->left != NULL)
+	{
+		pivot->left->parent = tree;
+	}
+	pivot->left = tree;
+	pivot->parent = tree->parent;
+	tree->parent = pivot;
+	return (pivot);
 }
