c27470280b42f3e9c5fa4f300c1f703c42abff25
Abdurleone <abyyyusuf@gmail.com>
2023-10-31 19:21:45 +0300

    binary

--- tests/100-main.c
+++ tests/100-main.c
@@ -12,7 +12,7 @@ void launch_test(binary_tree_t *n1, binary_tree_t *n2)
 {
     binary_tree_t *ancestor;
 
-    ancestor = binary_trees_ancestor(n1, n2);
+    ancestor = binary_tree_ancestor(n1, n2);
     printf("Ancestor of [%d] & [%d]: ", n1->n, n2->n);
     if (!ancestor)
         printf("(nil)\n");
