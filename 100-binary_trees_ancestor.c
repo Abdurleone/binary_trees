dd6e91f2738a40545ffe9d7da99832bf5011dd95
Abdurleone <abyyyusuf@gmail.com>
2023-10-31 09:25:53 +0300

    binary

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -10,28 +10,28 @@
 */
 
 binary_tree_t *binary_tree_ancensor(const binary_tree_t *first,
-                                    const binary_tree_t *second)
+		const binary_tree_t *second)
 {
-    binary_tree_t *p, *q;
+	binary_tree_t *p, *q;
 
-    if (first == NULL || second == NULL)
-    {
-        return (NULL);
-    }
-    if (first == second)
-    {
-        return ((binary_tree_t *)first);
-    }
+	if (first == NULL || second == NULL)
+	{
+		return (NULL);
+	}
+	if (first == second)
+	{
+		return ((binary_tree_t *)first);
+	}
 
-    p = first->parent;
-    q = second->parent;
-    if (p = NULL || first == q || (!p->parent && q))
-    {
-        return (binary_tree_ancensor(first, q));
-    }
-    else if (q == NULL || p == second || (!q->parent && p))
-    {
-        return (binary_tree_ancensor(p, second));
-    }
-    return (binary_tree_ancensor(p, q));
+	p = first->parent;
+	q = second->parent;
+	if (p = NULL || first == q || (!p->parent && q))
+	{
+		return (binary_tree_ancensor(first, q));
+	}
+	else if (q == NULL || p == second || (!q->parent && p))
+	{
+		return (binary_tree_ancensor(p, second));
+	}
+	return (binary_tree_ancensor(p, q));
 }
