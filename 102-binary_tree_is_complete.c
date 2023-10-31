b0394da5a74f71deda4fad90ad78de6f5d265a2d
Abdurleone <abyyyusuf@gmail.com>
2023-10-31 19:14:09 +0300

    binary

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -4,8 +4,8 @@
  * new_node - Function that creates a new_node in a linked_list
  * @node: Type pointer of node to be created
  * Return: The node created
-*/
-link_t *__new_node(binary_tree_t *node)
+ */
+link_t *new_node(binary_tree_t *node)
 {
 	link_t *new;
 
@@ -23,7 +23,7 @@ link_t *__new_node(binary_tree_t *node)
 /**
  * free_q - Function that frees the node at the linked list
  * @head: Node of the linked_list
-*/
+ */
 void free_q(link_t *head)
 {
 	link_t *temp_node;
@@ -41,7 +41,7 @@ void free_q(link_t *head)
  * @node: Type pointer of node of the tree
  * @head: Type head node of in the stack
  * @tail: Type tail node of in the stack
-*/
+ */
 void _push(binary_tree_t *node, link_t *head, link_t **tail)
 {
 	link_t *new;
@@ -59,7 +59,7 @@ void _push(binary_tree_t *node, link_t *head, link_t **tail)
 /**
  * _pop - Function that pops a node into the stack
  * @head: Type head of in the stack
-*/
+ */
 void _pop(link_t **head)
 {
 	link_t *temp_node;
@@ -73,7 +73,7 @@ void _pop(link_t **head)
  * binary_tree_is_complete - Function that checks if a binary tree is complete
  * @tree: Type pointer of node of the tree
  * Return: 1 if is complete 0 if it is not
-*/
+ */
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
 	link_t *head, *tail;
