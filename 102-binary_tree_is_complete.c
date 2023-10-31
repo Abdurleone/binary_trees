b854219dae5020c4aa200b0679e3e291ca1a494e
Abdurleone <abyyyusuf@gmail.com>
2023-10-31 12:42:12 +0300

    binary

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -7,17 +7,17 @@
 */
 link_t *__new_node(binary_tree_t *node)
 {
-    link_t *new;
+	link_t *new;
 
-    new = malloc(sizeof(link_t));
-    if (new == NULL)
-    {
-        return (NULL);
-    }
-    new->node = node;
-    new->next = NULL;
+	new = malloc(sizeof(link_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->node = node;
+	new->next = NULL;
 
-    return (new);
+	return (new);
 }
 
 /**
@@ -26,14 +26,14 @@ link_t *__new_node(binary_tree_t *node)
 */
 void free_q(link_t *head)
 {
-    link_t *temp_node;
+	link_t *temp_node;
 
-    while (head)
-    {
-        temp_node = head->next;
-        free(head);
-        head = temp_node;
-    }
+	while (head)
+	{
+		temp_node = head->next;
+		free(head);
+		head = temp_node;
+	}
 }
 
 /**
@@ -44,16 +44,16 @@ void free_q(link_t *head)
 */
 void _push(binary_tree_t *node, link_t *head, link_t **tail)
 {
-    link_t *new;
+	link_t *new;
 
-    new = new_node(node);
-    if (new == NULL)
-    {
-        free_q(head);
-        exit(1);
-    }
-    (*tail)->next = new;
-    *tail = new;
+	new = new_node(node);
+	if (new == NULL)
+	{
+		free_q(head);
+		exit(1);
+	}
+	(*tail)->next = new;
+	*tail = new;
 }
 
 /**
@@ -62,11 +62,11 @@ void _push(binary_tree_t *node, link_t *head, link_t **tail)
 */
 void _pop(link_t **head)
 {
-    link_t *temp_node;
+	link_t *temp_node;
 
-    temp_node = (*head)->next;
-    free(*head);
-    *head = temp_node;
+	temp_node = (*head)->next;
+	free(*head);
+	*head = temp_node;
 }
 
 /**
@@ -76,43 +76,43 @@ void _pop(link_t **head)
 */
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
-    link_t *head, *tail;
-    int flag = 0;
+	link_t *head, *tail;
+	int flag = 0;
 
-    if (tree == NULL)
-    {
-        return (0);
-    }
-    head = tail = new_node((binary_tree_t *)tree);
-    if (head == NULL)
-    {
-        exit(1);
-    }
-    while (head != NULL)
-    {
-        if (head->node->left != NULL)
-        {
-            if (flag == 1)
-            {
-                free_q(head);
-                return (0);
-            }
-            _push(head->node->left, head, &tail);
-        }
-        else
-        flag = 1;
-        if (head->node->right != NULL)
-        {
-            if (flag == 1)
-            {
-                free_q(head);
-                return (0);
-            }
-            _push(head->node->right, head, &tail);
-        }
-        else
-        flag = 1;
-        _pop(&head);
-    }
-    return (1);
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	head = tail = new_node((binary_tree_t *)tree);
+	if (head == NULL)
+	{
+		exit(1);
+	}
+	while (head != NULL)
+	{
+		if (head->node->left != NULL)
+		{
+			if (flag == 1)
+			{
+				free_q(head);
+				return (0);
+			}
+			_push(head->node->left, head, &tail);
+		}
+		else
+			flag = 1;
+		if (head->node->right != NULL)
+		{
+			if (flag == 1)
+			{
+				free_q(head);
+				return (0);
+			}
+			_push(head->node->right, head, &tail);
+		}
+		else
+			flag = 1;
+		_pop(&head);
+	}
+	return (1);
 }
